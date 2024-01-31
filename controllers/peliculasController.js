60293c9beef9437f6eaf3358218f0f402fa4aefb
sike1 <sikeo17@gmail.com>
2024-01-31 16:49:09 +0100

    fixes2

--- controllers/peliculasController.js
+++ controllers/peliculasController.js
@@ -193,6 +193,17 @@ exports.formEliminarPelicula = async (req, res, next) => {
 exports.eliminarPelicula = async (req, res) => {
   const pelicula = await Peliculas.findByPk(req.params.id);
 
+  if(pelicula.imagen){
+    const imagenAnteriorPath = __dirname +`/../public/uploads/carteles/${pelicula.imagen}`
+      //eliminar archivo cn fs
+      fs.unlink(imagenAnteriorPath, (error)=> {
+          if(error){
+              console.log(error)
+          }
+          return
+      })
+  }
+
   await pelicula.destroy();
 
   req.flash("exito", "Pelicula Eliminada");
